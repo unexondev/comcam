2f2a27f63a6c368573018d39dc5098f638081d66
Ahmet Celikbas <ahmetcelikbas333333@gmail.com>
2026-09-09 16:43:40 +0300

    Realsense specific profile matching function has been added to utilizations.

---
+++ util/profile/impl/realsense.py
@@ -0,0 +1,18 @@
+from pyrealsense2 import stream_profile as rs2_stream_profile
+from stream.profile import StreamProfile, VideoStreamProfile
+
+
+def is_profile_matching(sp : StreamProfile, sp_impl : rs2_stream_profile) -> bool:
+
+    if isinstance(sp, VideoStreamProfile):
+
+        if not sp_impl.is_video_stream_profile(): 
+            return False
+
+        # format is already being transformed
+        return (sp.width == sp_impl.width() and
+                sp.height == sp_impl.height() and
+                sp.fps == sp_impl.fps()) 
+
+    elif ...:
+        raise NotImplementedError() # TODO
