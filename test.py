cf7af46c981eb4ff588cb9842110bc38fac70ab7
Ahmet Celikbas <ahmetcelikbas333333@gmail.com>
2026-09-09 17:25:45 +0300

    minor changes.

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
 from pipeline import Pipeline, PipelineOptions
 from stream import VideoStreamProfile, StreamFormat
 
-pipe = Pipeline(PipelineOptions(
+import numpy
+
+
+ppl = Pipeline(PipelineOptions(
     # no options for now
 ))
 
-sp_depth = VideoStreamProfile(StreamFormat.DEPTH16, 1280, 720, 30)
+sp_depth = VideoStreamProfile(
+    format=StreamFormat.DEPTH16,
+    width=1280,
+    height=720,
+    fps=30
+    ) # define a stream profile of your choice
 
-pipe.add_config(
+ppl.add_config(
     profiles={ sp_depth }
-)
+) # pass it to the configuration
 
-pipe.start() # start pipeline
+ppl.start() # start receiving data
+
+while ppl.alive():
+
+    data : numpy.NDArray = ppl.stream(sp_depth).get()
+
+    # do something
