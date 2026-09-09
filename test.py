b16f041d88b6578b87e625b9bf233e3baf9bde13
Ahmet Celikbas <ahmetcelikbas333333@gmail.com>
2026-09-09 16:47:37 +0300

    Test file has been adapted to the new use of API.

--- test.py
+++ test.py
@@ -1,58 +1,14 @@
-from eyebin.stream import Stream
-from eyebin.stream.profile import VideoStreamProfile, StreamType, StreamFormat
-from eyebin.util.resolver import SPResolver
+from pipeline import Pipeline, PipelineOptions
+from stream import VideoStreamProfile, StreamFormat
 
-import numpy
-import cv2
+pipe = Pipeline(PipelineOptions(
+    # no options for now
+))
 
+sp_depth = VideoStreamProfile(StreamFormat.DEPTH16, 1280, 720, 30)
 
-# create stream profiles
-sp_depth = VideoStreamProfile(StreamType.depth, StreamFormat.z16, 1280, 720, 30)
-sp_color = VideoStreamProfile(StreamType.color, StreamFormat.rgba8, 1280, 720, 30)
+pipe.add_config(
+    profiles={ sp_depth }
+)
 
-# collect sensors
-# initialize a sensor resolver
-resolver = SPResolver()
-
-sensor_depth = resolver.resolve(sp_depth)
-sensor_color = resolver.resolve(sp_color)
-
-stream_depth = Stream()
-stream_color = Stream()
-
-sensor_depth.configure(stream=stream_depth, stream_profiles={sp_depth})
-sensor_color.configure(stream=stream_color, stream_profiles={sp_color})
-
-sensor_depth.start()
-sensor_color.start()
-
-stream_depth.wait_oldest(...)
-stream_color.wait_oldest(...)
-
-img = None
-
-# stream is consumable while it's active
-while stream.active():
-
-    frameset = stream.get_data()
-
-    if frameset is None:
-        continue
-
-    fcolor = frameset.get_color_frame() 
-
-    img = numpy.asanyarray(fcolor.get_data())
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-    
-
-    cv2.imshow("COLOR", img)
-
-    if cv2.waitKey(1) == 27:   # ESC
-        break
-
-# stop stream
-stream.stop()
-
-# close all windows
-cv2.destroyAllWindows()
+pipe.start() # start pipeline
